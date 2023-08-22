ed2f299190761625ef874be0fdd5f4ffb4fe7169
eduardo-thrash <eduardo-thrash@hotmail.com>
2023-08-21 23:28:21 -0500

    Primary Commit for Project Template Configuration with Serenity BDD JS and Mocha

---
+++ spec/api/example.spec.ts
@@ -0,0 +1,15 @@
+import "mocha";
+
+describe("Bloque principal", () => {
+  describe("Bloque secundario 1", () => {
+    it("Prueba 1", () => console.log("Prueba 1"));
+  });
+
+  describe("Bloque secundario 2", () => {
+    it("Prueba 1", () => console.log("Prueba 2"));
+  });
+
+  describe("Bloque secundario 3", () => {
+    it("Prueba 1", () => console.log("Prueba 3"));
+  });
+});
